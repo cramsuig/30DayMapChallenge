ad46f4773e17ee7593f595ae0bfc202911935387
cramsuig <csuasteguir@live.com>
2021-11-12 01:54:00 +0000

    squized two into one. Ha!

---
+++ 2021/code/2021-11-11.R
@@ -0,0 +1,85 @@
+#!/usr/bin/R
+
+# ------------------------------------------------------------------------------
+# title: 3D.
+# purpose: This script is a template of best practices for an R script.
+# author:
+#   - name: Ciro Ramírez-Suástegui
+#     affiliation: La Jolla Institute for Immunology
+#     email: ksuastegui@gmail.com
+# date: 2021-11-11
+# ------------------------------------------------------------------------------
+# Thanks to:
+# https://rspatialdata.github.io/elevation.html
+# Might wanna look at:
+# https://rpubs.com/ials2un/elevationfromR
+
+packages_funcs = c(
+  "tidyverse",
+  "osmdata",
+  "tylermorganwall/rayshader",
+  "elevatr",
+  "raster",
+  "rtweet"
+)
+source(here::here("2021", "code", "installations.R"))
+day <- "2021-11-11"
+fname <- here::here("2021", "img", day)
+
+# mapdata <- rgeoboundaries::geoboundaries("Mexico")
+mapdata <- opq(bbox = "Guerrero, Mexico", timeout = 60) %>%
+  add_osm_feature(key = "admin_level", value = "4") %>%
+  osmdata_sf() %>%
+  .$osm_multipolygons %>%
+  dplyr::select(osm_id, name, geometry) %>%
+  filter(name %in% "Guerrero")
+
+mape <- elevatr::get_elev_raster(locations = mapdata, z = 9, clip = "locations")
+mape_df <- tibble::as_tibble(as.data.frame(mape, xy = TRUE)) %>% drop_na()
+colnames(mape_df)[3] <- "elevation"
+
+p <- ggplot() +
+  geom_raster(data = mape_df, aes(x = x, y = y, fill = elevation)) +
+  geom_sf(data = mapdata, fill = NA, color = NA) +
+  scale_fill_distiller() +
+  labs(fill = NULL,
+       title = "State of Guerrero, México",
+       caption = paste0("#30DayMapChallenge | Day 10, raster | ",
+                        "@cramsuig | Data: OpenStreetMap, AWS Open Data Terrain Tiles")) +
+  theme_void() +
+  theme(legend.position = "none",
+        panel.background = element_rect(fill = '#ffffff', color = NA),
+        plot.background=element_rect(fill = '#ffffff', color = NA),
+        plot.caption = element_text(face = "bold", size = 4, hjust = 0.5, color = "gray50"),
+        plot.title = element_text(family = "Zapfino", size = 8, hjust = 0.5))
+
+ggplot2::ggsave(
+  filename = here::here("2021", "img", "2021-11-10.png"),
+  plot = p +
+    theme(plot.caption = element_text(face = "bold", size = 12, hjust = 0.5, color = "gray50"),
+          plot.title = element_text(family = "Zapfino", size = 27, hjust = 0.5))
+)
+
+# https://github.com/ropensci/rtweet/issues/486
+rtweet::post_tweet(
+  status = paste0(
+    "#30DayMapChallenge raster\n\n",
+    "code: ", gsub("\\.git", "/", usethis::git_remotes()), "blob/main/2021/code/", day, ".R"
+  ),
+  media = here::here("2021", "img", "2021-11-10.png"),
+  media_alt_text = "Surface elevation of the state of Guerrero, México."
+)
+
+rayshader::plot_gg(p, phi = 50, sunangle = 90, theta = 0, zoom = 0.55)
+rayshader::render_snapshot(fname)
+rayshader::render_highquality(paste0(fname, ".png")) # looks too dark :(
+
+rtweet::post_tweet(
+  status = paste0(
+    "#30DayMapChallenge 3D\n\n",
+    "code: ", gsub("\\.git", "/", usethis::git_remotes()), "blob/main/2021/code/", day, ".R",
+    "\n\nDidn't have time to play more with the quality. Maybe another day."
+  ),
+  media = paste0(fname, ".png"),
+  media_alt_text = "3D surface elevation of the state of Guerrero, México."
+)
--- 2021/code/installations.R
+++ 2021/code/installations.R
@@ -10,26 +10,29 @@
 # ------------------------------------------------------------------------------
 
 renv::activate(here::here("2021"))
-packages_funcs = c(
-  "dplyr",
-  "ggplot2",
-  "osmdata", # 2021-11-07
-  "sf",
-  "sp",
-  "sysfonts",
-  "remotes", "raster", "spData", "tmap", "leaflet",
-  "ggrastr", # had to download https://www.xquartz.org
-  "cowplot",
-  "Nowosad/spDataLarge",
-  "devtools", # 2021-11-08
-  "thomasp85/ambient@v1.0.0",
-  "gridGraphics",
-  "ggpubr", "magick", "rnaturalearth", "rgdal",
-  "afrimapr/afrilearndata", # 2021-11-09
-  "afrimapr/afrihealthsites", # updated glue and tibble, not compiled systemfonts
-  "ropensci/rtweet", # somehow I had to do it manually again :(
-  "gganimate", "transformr"
-)
+if(!exists("packages_funcs")){
+  packages_funcs = c(
+    "tidyverse",
+    "osmdata", # 2021-11-07
+    "sf",
+    "sp",
+    "sysfonts",
+    "remotes", "raster", "spData", "tmap", "leaflet",
+    "ggrastr", # had to download https://www.xquartz.org
+    "cowplot",
+    "Nowosad/spDataLarge",
+    "devtools", # 2021-11-08
+    "thomasp85/ambient@v1.0.0",
+    "gridGraphics",
+    "ggpubr", "magick", "rnaturalearth", "rgdal",
+    "afrimapr/afrilearndata", # 2021-11-09
+    "afrimapr/afrihealthsites", # updated glue and tibble, not compiled systemfonts
+    "ropensci/rtweet", # somehow I had to do it manually again :(
+    "gganimate", "transformr",
+    "tylermorganwall/rayshader", # 2021-11-11
+    "elevatr"
+  )
+}
 i <- !gsub(".*\\/|@.*", "", packages_funcs) %in% installed.packages()[, "Package"]
 if(any(i)) cat(crayon::red("Not installed:"), packages_funcs[i], sep = "\n ")
 for (i in packages_funcs){
@@ -37,7 +40,7 @@ for (i in packages_funcs){
   i_name <- gsub(".*\\/|@.*", "", i)
   if(!file.exists(i)){
     if (!requireNamespace(i_name, quietly = TRUE)){
-      cat(" - installing")
+      cat(" - installing\n")
       if(grepl("\\/", i)){
         remotes::install_github(i) # install from github if it's not on CRAN
         # install.packages("spDataLarge", repos = "https://nowosad.github.io/drat/", type = "source")
@@ -48,4 +51,4 @@ for (i in packages_funcs){
     suppressMessages(require(package = i_name, quietly = TRUE, character.only = TRUE))
   }else{ source(i) }
 }
-rm(i, i_name, packages_funcs)
+rm(packages_funcs, i, i_name)
